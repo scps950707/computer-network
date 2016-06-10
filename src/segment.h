41c3021ce1d064ee7889061f235e4e684db72223
scps950707 <scps950707@gmail.com>
2016-06-11 01:37:17 +0800

    add constructor for Packet

--- src/segment.h
+++ src/segment.h
@@ -2,7 +2,7 @@
  * Author:         scps950707
  * Email:          scps950707@gmail.com
  * Created:        2016-06-10 16:57
- * Last Modified:  2016-06-10 18:26
+ * Last Modified:  2016-06-11 01:11
  * Filename:       segment.h
  * Purpose:        homework
  */
@@ -12,24 +12,55 @@
 
 #include "stdint.h"
 
-typedef struct __attribute__( ( packed ) )
+typedef struct tmp
 {
-    uint16_t sourcePort = 0;
-    uint16_t destPort = 0;
-    uint32_t seqNum = 0;
-    uint32_t ackNum = 0;
-    bool URG = false;
-    bool ACK = false;
-    bool PSH = false;
-    bool RST = false;
-    bool SYN = false;
-    bool FIN = false;
-    uint16_t recvWindowl = 0;
-    uint16_t checkSum = 0;
-    uint16_t urgDataPtr = 0;
-    uint32_t options = 0;
+    uint16_t sourcePort;
+    uint16_t destPort;
+    uint32_t seqNum;
+    uint32_t ackNum;
+    bool URG;
+    bool ACK;
+    bool PSH;
+    bool RST;
+    bool SYN;
+    bool FIN;
+    uint16_t recvWindowl;
+    uint16_t checkSum;
+    uint16_t urgDataPtr;
+    uint32_t options;
     char appData[484];
-}
+    tmp()
+    {
+        sourcePort = 0;
+        destPort = 0;
+        seqNum = 0;
+        ackNum = 0;
+        URG = false;
+        ACK = false;
+        PSH = false;
+        RST = false;
+        SYN = false;
+        FIN = false;
+        recvWindowl = 0;
+        checkSum = 0;
+        urgDataPtr = 0;
+        options = 0;
+    }
+    tmp( uint16_t sourcePort, uint16_t destPort, uint32_t seqNum, uint32_t ackNum )
+        : sourcePort( sourcePort ), destPort( destPort ), seqNum( seqNum ), ackNum( ackNum )
+    {
+        URG = false;
+        ACK = false;
+        PSH = false;
+        RST = false;
+        SYN = false;
+        FIN = false;
+        recvWindowl = 0;
+        checkSum = 0;
+        urgDataPtr = 0;
+        options = 0;
+    }
+} __attribute__( ( packed ) )
 Packet;
 
 #endif
