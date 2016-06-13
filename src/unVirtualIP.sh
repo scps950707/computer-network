bb42d9dfc08908cfc55f34159e62bec27d39e34c
scps950707 <scps950707@gmail.com>
2016-06-13 11:10:53 +0800

    add unVirtualIP.sh

---
+++ src/unVirtualIP.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo ifconfig eth0:0 192.168.0.1 down
+sudo ifconfig eth0:1 192.168.0.2 down
+sudo ifconfig eth0:2 192.168.0.3 down
+sudo ifconfig eth0:3 192.168.0.4 down
+sudo ifconfig eth0:4 192.168.0.5 down
